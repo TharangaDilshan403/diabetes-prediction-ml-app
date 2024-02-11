769e16c794313a089192fd6ed1eede5135d82e29
Tharanga_Dilshan <59679693+TharangaDilshan403@users.noreply.github.com>
2024-02-11 13:01:50 +0530

    angular fe form ui

--- ml_ijse_fe/src/app/app.component.ts
+++ ml_ijse_fe/src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardFooter,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardContent, MatCardFooter, MatCardActions, MatCardTitle, MatCardSubtitle, MatCardHeader, MatCard, MatToolbar, MatLabel, MatFormField, MatInput, ReactiveFormsModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'ml_ijse_fe';
+
+  diabetesCalculationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.diabetesCalculationForm = this.formBuilder.group({
+      pregnancies: ['', Validators.required],
+      glucose: ['', [Validators.required]],
+      bloodPressure: ['', [Validators.required]],
+      skinThickness: ['', [Validators.required]],
+      insulin: ['', [Validators.required]],
+      bmi: ['', [Validators.required]],
+      diabetesPedigree: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+    });
+  }
+  calculate() {
+    console.log('Form values:', this.diabetesCalculationForm.value);
+  }
+
 }
